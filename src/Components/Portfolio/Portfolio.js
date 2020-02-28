8f9af1a33c8965ebb6da990af0e880a287eb4037
Aidan <akinzett@gmail.com>
2020-02-28 20:58:45 +1000

    centered portfolio items

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -5,7 +5,7 @@ import PortfolioItem from './PortfolioItem/PortfolioItem'
 
 function Portfolio(props) {
   return (
-    <Box direction="column" gap="medium" pad="large" justify="between" style={{maxWidth: 800}}>
+    <Box direction="column" gap="medium" pad="large" justify="between" style={{maxWidth: 800}} alignSelf="center">
       <Heading>Portfolio</Heading>
       {props.portfolioItems.map(item => <PortfolioItem item={item} />)}
     </Box>
